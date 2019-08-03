679d7a008b3e483fd1977ebd4a043a124a09cb37
Bo Henderson <bohende@gmail.com>
2019-08-03 15:09:19 +0530

    fix express pipeline

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ app.use(express.json())
 app.use((req, res, next) => {
   console.log(`=> ${req.method} ${req.path} -- ${JSON.stringify(req.query)}`)
   if (JSON.stringify(req.body) !== "{}") console.log(`body: ${JSON.stringify(req.body)}`)
-  res.status(200).send('Cool story, Bro')
 })
 
 app.get('/webhooks/twitter', (req, res, next) => {
@@ -20,6 +19,10 @@ app.get('/webhooks/twitter', (req, res, next) => {
   const response_token = `sha256=${hmac.digest('base64')}`
   console.log(`Got CRC, responding with: ${response_token}`)
   res.status(200).json({ response_token })
+})
+
+app.all('*', (req, res) => {
+  res.status(200).send('Cool story, Bro')
 })
 
 app.listen(port, () => {

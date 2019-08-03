e308308c895019bf8324cf803515917787366b27
Bo Henderson <bohende@gmail.com>
2019-08-03 16:27:33 +0530

    don't print config on startup anymore

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ const app = express()
 
 const port = process.env.PORT || '8080'
 
-console.log(`Starting w config: ${JSON.stringify(config, null, 2)}`)
-
 app.use(express.json())
 
 app.use((req, res, next) => {

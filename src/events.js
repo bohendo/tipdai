e8730794ccb4d137c51ede14e4f9a40ad053dd4b
Bo Henderson <bohende@gmail.com>
2019-08-03 16:57:55 +0530

    better event log

--- src/events.js
+++ src/events.js
@@ -1,9 +1,9 @@
 const handleTweet = (tweet) => {
-  console.log(`Got a tweet event: ${JSON.stringify(tweet)}`)
+  console.log(`Got a tweet event: ${JSON.stringify(tweet, null, 2)}`)
 }
 
 const handleMessage = (message) => {
-  console.log(`Got a message event: ${JSON.stringify(message)}`)
+  console.log(`Got a message event: ${JSON.stringify(message, null, 2)}`)
 }
 
 module.exports = { handleMessage, handleTweet }

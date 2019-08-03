eb29679538d2be84c91310beb8d7a57d3efaba71
Bo Henderson <bohende@gmail.com>
2019-08-03 12:52:14 +0530

    rm some logs

--- src/index.js
+++ src/index.js
@@ -9,17 +9,10 @@ const port = process.env.PORT || '8080'
 app.use(express.json())
 
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path}`)
-  console.log(`query: ${JSON.stringify(req.query)}`)
-  console.log(`body: ${JSON.stringify(req.body)}`)
+  console.log(`${req.method} ${req.path} query: ${JSON.stringify(req.query)}`)
+  if (req.body) console.log(`body: ${JSON.stringify(req.body)}`)
   console.log(``)
-  next()
-})
-
-app.all('*', (req, res, next) => {
-  const reply = 'Cool story, Bro'
-  console.log(`Sending reply: ${reply}`)
-  res.status(200).send(reply)
+  res.status(200).send('Cool story, Bro')
 })
 
 app.listen(port, () => {

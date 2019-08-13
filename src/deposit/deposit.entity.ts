be933772c5ad27342069e8c438bc384fe6ae27c7
Bo Henderson <bohende@gmail.com>
2019-08-13 17:47:09 +0530

    init deposit module w typeorm entity

---
+++ src/deposit/deposit.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Deposit {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column('citext')
+  address!: string;
+
+  @Column('citext')
+  amount!: string;
+
+  @Column('citext')
+  startTime!: string;
+
+  @Column('citext')
+  user!: string;
+
+  @Column('citext')
+  oldBalance!: string;
+}
--- src/deposit/deposit.module.ts
+++ src/deposit/deposit.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { DepositService } from './deposit.service';
 
 @Module({
-  providers: [DepositService]
+  providers: [DepositService],
 })
 export class DepositModule {}

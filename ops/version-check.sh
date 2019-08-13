fec0a09f32a097fb4952559e471019c13195d917
Bo Henderson <bohende@gmail.com>
2019-08-13 16:12:58 +0530

    upgrade stuff

--- ops/version-check.sh
+++ ops/version-check.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
+function hr {
+  for i in {1..58}
+  do echo -n "-"
+  done
+  echo
+}
+
 format='{printf("| %-32s|%8s  ->  %-8s|\n", $1, $3, $4)}'
-npm outdated -D | tail -n +2 | awk '$3 != $4' | awk "$format"
+echo;echo "  Checking depencencies"
+hr
 npm outdated -S | tail -n +2 | awk '$3 != $4' | awk "$format"
+hr
+echo;echo "  Checking devDepencencies"
+hr
+npm outdated -D | tail -n +2 | awk '$3 != $4' | awk "$format"
+hr
+echo;echo "Done!"

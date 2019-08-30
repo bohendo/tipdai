9f5fad7263fd1338b8a52833117849b3d3fcde59
Bo Henderson <bohende@gmail.com>
2019-08-30 15:34:25 +0200

    move getHello logic into service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,35 +1,13 @@
-import { Body, Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
 import { AppService } from './app.service';
-import { ConfigService } from './config/config.service';
-import { TwitterService } from './twitter/twitter.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly config: ConfigService,
-    private readonly twitter: TwitterService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getHello(@Query() query: any, @Body() body: any): Promise<string> {
-    console.log(`Body: ${JSON.stringify(body)}`);
-    console.log(`Query: ${JSON.stringify(query)}`);
-    if (query.oauth_token && query.oauth_verifier) {
-      console.log(`Oauth response detected`);
-      const tokenRes = await this.twitter.getAccessToken(
-        this.config.twitterDev.consumerKey,
-        query.oauth_token,
-        query.oauth_verifier,
-      );
-      console.log(`Got access token: ${JSON.stringify(tokenRes)}`);
-      console.log(`(You should save this for later)`);
-      await this.twitter.connectBot(
-        tokenRes.oauth_token,
-        tokenRes.oauth_token_secret,
-      );
-    }
-    return this.appService.getHello(this.twitter.authUrl);
+  async getHello(@Query() query: any): Promise<string> {
+    return await this.appService.getHello(query);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,35 @@
 import { Injectable } from '@nestjs/common';
 
+import { ConfigService } from './config/config.service';
+import { TwitterService } from './twitter/twitter.service';
+
 @Injectable()
 export class AppService {
-  getHello(authUrl): string {
-    if (authUrl) {
-      return `Hello World!<br/>Join the tip bot army: ${authUrl}`;
+  constructor(
+    private readonly config: ConfigService,
+    private readonly twitter: TwitterService,
+  ) {}
+  async getHello(query: any): Promise<string> {
+
+    console.log(`Query: ${JSON.stringify(query)}`);
+    console.log(`Twitter: ${typeof this.twitter}`);
+    if (query.oauth_token && query.oauth_verifier) {
+      console.log(`Oauth response detected`);
+      const tokenRes = await this.twitter.getAccessToken(
+        this.config.twitterDev.consumerKey,
+        query.oauth_token,
+        query.oauth_verifier,
+      );
+      console.log(`Got access token: ${JSON.stringify(tokenRes)}`);
+      console.log(`(You should save this for later)`);
+      await this.twitter.connectBot(
+        tokenRes.oauth_token,
+        tokenRes.oauth_token_secret,
+      );
+    }
+
+    if (this.twitter.authUrl) {
+      return `Hello World!<br/>Join the tip bot army: ${this.twitter.authUrl}`;
     }
     return 'Hello World!';
   }

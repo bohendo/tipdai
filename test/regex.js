21f6b6eac3b6dbbd523935b792c158c42ad06980
Bo Henderson <bohende@gmail.com>
2019-11-09 00:24:01 +0900

    typo fix

--- test/regex.js
+++ test/regex.js
@@ -1,42 +1,44 @@
 const constants = require('../dist/constants');
 
-
-
 for (const dm of [
   {
-    msg: '@recipient @Bot @Bot send @recipient $5 please.',
+    msg: '@nanexcool @antiprosynth Well get there indeed.\n\nHeres a bit of testnet dai to give you a taste of a future where people might give a fuck about Dai w/out leaving the comfort of Twitter.\n\n@TipDai give @nanexcool $1 please.',
+    expected: { recipient: 'nanexcool', amount: '1' }
+  },
+  {
+    msg: '@recipient @TipDai @TipDai send @recipient $5 please.',
     expected: { recipient: 'recipient', amount: '5' }
   },
   {
-    msg: '@recipient @Bot @Bot send @recipient $0.10 please.',
+    msg: '@recipient @TipDai @TipDai send @recipient $0.10 please.',
     expected: { recipient: 'recipient', amount: '0.10' }
   },
   {
-    msg: '@Bot Hi, send @recipient (not @invalid) some money: $0.101.',
+    msg: '@TipDai Hi, send @recipient (not @invalid) some money: $0.101.',
     expected: { recipient: 'recipient', amount: '0.10' }
   },
   {
-    msg: '@Bot Hi, send @recipient (not @invalid) some money: $0.10.',
+    msg: '@TipDai Hi, send @recipient (not @invalid) some money: $0.10.',
     expected: { recipient: 'recipient', amount: '0.10' }
   },
   {
-    msg: '@Bot Hi, send @recipient (not @invalid) some money: $0.10',
+    msg: '@TipDai Hi, send @recipient (not @invalid) some money: $0.10',
     expected: { recipient: 'recipient', amount: '0.10' }
   },
   {
-    msg: '@Bot Hi, send @recipient $0.10 and give @invalid like idk $100',
+    msg: '@TipDai Hi, send @recipient $0.10 and give @invalid like idk $100',
     expected: { recipient: 'recipient', amount: '0.10' }
   },
   {
-    msg: '@Bot Hi, send @recipient some money: $0.10 or else!',
+    msg: '@TipDai Hi, send @recipient some money: $0.10 or else!',
     expected: { recipient: 'recipient', amount: '0.10' }
   },
   {
-    msg: '@Bot Hi, send @recipient some money: $0.10',
+    msg: '@TipDai Hi, send @recipient some money: $0.10',
     expected: { recipient: 'recipient', amount: '0.10' }
   }
 ]) {
-  const actual = dm.msg.match(constants.tipRegex('Bot'))
+  const actual = dm.msg.match(constants.tipRegex('TipDai'))
   if (!actual || !actual[2]) {
     throw new Error(`Expected a match for "${dm.msg}" but got ${JSON.stringify(actual)}`);
   }

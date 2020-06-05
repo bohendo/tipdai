c69ca64783f092cceab6bff1587c35ef1ec96401
bohendo <bohende@gmail.com>
2020-06-05 10:46:27 +0530

    🐛 fix bot entry.sh

--- ops/bot/entry.sh
+++ ops/bot/entry.sh
@@ -3,16 +3,18 @@ set -e
 
 if [[ "$NODE_ENV" == "production" ]]
 then
-  node dist/main.js
+  echo "Starting bot in prod-mode"
+  node "dist/main.js"
 else
-  nodemon
-    --delay=1 \
-    --exitcrash \
-    --ignore=*.test.ts \
-    --ignore=*.swp \
-    --legacy-watch \
-    --polling-interval=1000 \
-    --watch=src \
-    --exec=ts-node \
-    ./src/main.ts
+  echo "Starting bot in dev-mode"
+  nodemon \
+    "--delay=1" \
+    "--exitcrash" \
+    "--ignore=*.test.ts" \
+    "--ignore=*.swp" \
+    "--legacy-watch" \
+    "--polling-interval=1000" \
+    "--watch=src" \
+    "--exec=ts-node" \
+    "./src/main.ts"
 fi
